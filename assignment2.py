f45370cbeee32e81a9e53b79a5e11082202f5d47
Vishwa Mithra <mithravishwa37@gmail.com>
2022-03-14 01:10:31 +0530

    Assignment2_flask_file

--- assignment2.py
+++ assignment2.py
@@ -8,7 +8,7 @@ def index():
 
     host = flask.request.host
     hostname = platform.platform()
-    return "Serving from"+"\n"+"Host: "+host+"\n"+"Hostname: "+hostname
+    return "Serving from"+"<br/>"+"Host: "+host+"<br/>"+"Hostname: "+hostname
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080,debug=True)
