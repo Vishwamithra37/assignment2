f399e0e1d5b082d8969ebf017bc7d499832f1b45
Vishwa Mithra <mithravishwa37@gmail.com>
2022-03-16 01:54:25 +0530

    Added time

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,6 @@
 import flask
 import platform
+import time
 
 app = flask.Flask(__name__)
 
@@ -8,7 +9,8 @@ def index():
 
     host = flask.request.host
     hostname = platform.platform()
-    return "Serving from"+"<br/>"+"Host: "+host+"<br/>"+"Hostname: "+hostname
+    Time=time.strftime("%H:%M:%S")
+    return "Serving from"+"<br/>"+"Host: "+host+"<br/>"+"Hostname: "+hostname+"<br/>"+"Time: "+Time
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080,debug=True)
