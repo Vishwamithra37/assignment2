5ec312ca69155693ca27f621ac95142425bc8ae0
Vishwamithra37 <53423141+Vishwamithra37@users.noreply.github.com>
2022-03-25 11:03:14 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -13,4 +13,4 @@ def index():
     return "Serving from"+"<br/>"+"Host: "+host+"<br/>"+"Hostname: "+hostname+"<br/>"+"Time: "+Time
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080,debug=True)
+    app.run(host='0.0.0.0', port=80,debug=True)
